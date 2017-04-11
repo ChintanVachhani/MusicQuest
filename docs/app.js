324b6003386e7e28bed95e944f8e7a7fab214751
ChintanVachhani <chintan.vachhani10@gmail.com>
2017-04-11 14:28:29 -0700

    added docs folder for hosting

---
+++ docs/app.js
@@ -0,0 +1,68 @@
+'use strict';
+// App level module
+angular.module('musicQuestApp', [
+    'ngRoute'
+]).controller('displayViewController', function ($scope, $http) {
+    $scope.tracksList = [];
+    $scope.searchString = '';
+    $scope.prevResults = '';
+    $scope.nextResults = '';
+    $scope.searchSuccessful = false;
+    $scope.yearString = '';
+    $scope.genreString = '';
+    $scope.filters = '';
+
+    // Function to get track search results on clicking Search button
+    $scope.searchSpotify = function () {
+        if ($scope.searchString !== '') {
+            if ($scope.yearString !== '') {
+                $scope.filters += "%20year:" + $scope.yearString;
+            }
+            if ($scope.genreString !== '') {
+                $scope.filters += "%20genre:" + $scope.genreString;
+            }
+            $scope.requestURIString = "https://api.spotify.com/v1/search?q=" + $scope.searchString + $scope.filters + "&type=track";
+            $scope.makeGETRequest($scope.requestURIString);
+        }
+    };
+
+    // Function to get next tracks for the search results on clicking Next button
+    $scope.getPreviousTracks = function () {
+        $scope.makeGETRequest($scope.prevResults);
+    };
+
+    // Function to get previous tracks for the search results on clicking Previous button
+    $scope.getNextTracks = function () {
+        $scope.makeGETRequest($scope.nextResults);
+    };
+
+    // Main Function that makes GET request to the search on Spotify Web-API
+    $scope.makeGETRequest = function (requestURI) {
+        if (requestURI) {
+            $scope.tracksList = [];
+            $http.get(requestURI).success(function (res) {
+                if (res.tracks.total > 0) {
+                    $scope.prevResults = res.tracks.previous;
+                    $scope.nextResults = res.tracks.next;
+                    for (let i = 0; i < res.tracks.items.length; ++i) {
+                        $scope.tracksList.push($scope.getNewTrack(res.tracks.items[i]));
+                    }
+                    $scope.searchSuccessful = true;
+                } else {
+                    $scope.searchSuccessful = false;
+                }
+            });
+        }
+    };
+
+    // Function to create a new track object to be added to a row in the table
+    $scope.getNewTrack = function (obj) {
+        let newTrack = {id: '', name: '', albumName: '', duration: '', popularity: ''};
+        newTrack.id = obj.id;
+        newTrack.name = obj.name;
+        newTrack.albumName = obj.album.name;
+        newTrack.duration = obj.duration_ms / 1000;
+        newTrack.popularity = obj.popularity;
+        return newTrack;
+    };
+});
